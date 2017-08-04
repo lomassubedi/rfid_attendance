59ad4adf118166dcd0443ab96c443488837abeec
lomassubedi <subedilomas@gmail.com>
2017-08-04 11:25:39 +0545

    Just done with queue, yet to test in detail.

---
+++ RFID_NODE/customCodes.h
@@ -0,0 +1,12 @@
+/***
+ * Error codes
+ */
+#define     ERROR_JSON_PARSE      2
+#define     ERROR_SUCESS_FALSE    3
+#define     ERROR_CODE            0
+
+/***
+ * Non error codes
+ */
+#define     SUCESS_CODE           1
+#define     UPLOAD_SUCESS_TRUE    SUCESS_CODE
